25e904fac4b58600fabe73eb6f6b7a885338494e
= <=>
2020-04-05 12:24:51 +0200

    Refactored tests files

--- tests/auth.api.test.js
+++ tests/auth.api.test.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-undef */
 const request = require('supertest');
-const app = require('../app'); //reference to you app.js file
+const app = require('../app');
+// reference to you app.js file
 const req = request(app);
 
 describe('/api/auth', () => {
-    it('Gets a message', (done) => {
-        req
-            .get('/api/auth')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
-    });
-
+  it('Gets a message', (done) => {
+    req
+      .get('/api/auth')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
 });
--- tests/user.api.test.js
+++ tests/user.api.test.js
@@ -1,13 +1,15 @@
+/* eslint-disable no-undef */
 const request = require('supertest');
-const app = require('../app'); //reference to you app.js file
+const app = require('../app');
+// reference to you app.js file
 const req = request(app);
 
-describe('/api/user - Get users without token', function () {
-    it('Gets bad request', function (done) {
-        req
-            .post('/api/user')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(400, done);
-    })
+describe('/api/user - Get users without token', () => {
+  it('Gets bad request', (done) => {
+    req
+      .post('/api/user')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400, done);
+  });
 });
