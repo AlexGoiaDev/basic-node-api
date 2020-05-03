bb585e7636008236397e1dbbd07d04cf33c712e2
pite <pedro.clemente.martinez@gmail.com>
2020-05-03 19:59:18 +0200

    try to fix some tsLint problems

--- routes/api/TESTACCESO.js
+++ routes/api/TESTACCESO.js
@@ -1,32 +1,36 @@
 const express = require('express');
-var router = express.Router();
-var pgp = require("pg-promise")();
-var db = pgp("postgres://postgres:mysecretpassword@192.168.0.12:5432/postgres");
+
+const pgp = require("pg-promise")();
+
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+const db = pgp('postgres://postgres:mysecretpassword@192.168.0.12:5432/postgres');
 //this shit is only to can read de body content
-var app = express();
-var bodyParser = require('body-parser');
+
+const app = express();
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 router
-.get('/', function(req, res, next) {
-    db.any("select * from public.account",).then(data=>{  
-        if(data){
-          data.forEach(element => {
-          console.log(element.username) 
-        }); 
-    }}
+  .get('/', function(req, res, next) { 
+    db.any('select * from public.account',).then(data=>{  
+      if(data){
+        data.forEach(element => {
+          console.log(element.username);
+        });
+      }}
     ).catch(error=>console.log(error))
     .finally(res.send('ok'));
 });
 
 router.post('/', function(req,res,next){
-    console.log(req);
-    db.one("insert into public.account(user_id,username) values($1,$2)",[req.body.userid,req.body.username])
-    .then(data=>console.log(data)).catch(error=>console.log(error)).finally();
-    res.send();
+  console.log(req);
+  db.one('insert into public.account(user_id,username) values($1,$2)',[req.body.userid,req.body.username])
+  .then(data=>console.log(data)).catch(error=>console.log(error)).finally();
+  res.send();
 });
 
   
 module.exports = router;
-  
