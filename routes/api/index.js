92ad411b8af88d9bd891102426701ec7a2787ef8
Alex Goia <alexgoiadev@gmail.com>
2020-05-07 23:43:16 +0200

    Removed auth for al payments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const isAuth = require('../../middlewares/isAuth');
 
 router.use('/google-auth', require('./googleAuth'));
 router.use('/user', require('./user'));
 router.use('/auth', require('./auth'));
 router.use('/me', require('./me'));
-router.use('/payment', isAuth, require('./payment'));
+router.use('/payment', require('./payment'));
 
 module.exports = router;
