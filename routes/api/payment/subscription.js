d50b7263fa60003ba002e1b701f286d8edd7aea2
Alex Goia <alexgoiadev@gmail.com>
2020-06-14 16:06:56 +0200

    Reactivate subscription

--- routes/api/payment/subscription.js
+++ routes/api/payment/subscription.js
@@ -40,4 +40,41 @@ router.delete('/:id', async (req, res, next) => {
     return next(err);
   }
 });
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    // TODO: A cliente shoud only cancel their subscriptions
+    const { id } = req.params;
+    const { reactivate } = req.body;
+    if (!id || !reactivate) {
+      throw new BadRequestError('Subscription id an reactivate option is required');
+    }
+
+    const user = await User.findById({ _id: req.user._id });
+    if (!user) {
+      throw new Error('Unknown error');
+    }
+    // const customer = await stripe.customers.retrive(user.stripeCustomerId);
+    const customer = await stripe.customers.retrieve(user.stripeCustomerId);
+    const subscription = customer.subscriptions.data.filter((s) => s.id === id);
+    if (!subscription) {
+      throw new BadRequestError("User don't have that subscription");
+    }
+
+    const suscriptionDeleted = await stripe.subscriptions.update(
+      id,
+      { cancel_at_period_end: !!reactivate },
+    );
+
+    if (!suscriptionDeleted) {
+      throw new NoContentError('No suscription with that id');
+    }
+
+    return res.send(suscriptionDeleted);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
 module.exports = router;
