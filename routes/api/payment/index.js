8e9fab764d82770cc2568a58029a4cb3f6d5ca16
Alex Goia <alexgoiadev@gmail.com>
2020-06-23 15:06:18 +0200

    Used stripe enabled

--- routes/api/payment/index.js
+++ routes/api/payment/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const isAuth = require('../../../middlewares/isAuth');
-
+const isStripeEnabled = require('../../../middlewares/isStripeEnabled');
 router.use('/customer', isAuth, require('./customer'));
 router.use('/session', isAuth, require('./session'));
-router.use('/plans', require('./plans'));
+router.use('/plans', isStripeEnabled, require('./plans'));
 router.use('/subscription', isAuth, require('./subscription'));
 
 module.exports = router;
